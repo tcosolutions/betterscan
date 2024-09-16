22ae18c71faaf032bd2c7a22106c52ce3723da3c
Marcin Kozlowski <marcinguy@gmail.com>
2024-09-16 10:45:11 +0200

    Update

--- cli.sh
+++ cli.sh
@@ -4,7 +4,7 @@ cd $CODE_DIR
 # Set environment variables for Docker
 ENV_VARS="-e CODE_DIR -e OPENAI_GPT_API"
 VOLUME_MOUNT="-v ${PWD}:${PWD}"
-DOCKER_IMAGE="sunsolution/betterscan-worker-cli:latest"
+DOCKER_IMAGE="tcosolutions/betterscan-worker-cli:latest"
 SAFE_DIR_CMD="git config --global --add safe.directory $CODE_DIR"
 GIT_BRANCH_CMD="git rev-parse --abbrev-ref HEAD"
 
